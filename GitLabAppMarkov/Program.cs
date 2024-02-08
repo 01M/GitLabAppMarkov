fe88547116bdac87738698c04594517d26dd9a12
Stepan Vatavu Vladimir Dzura <is26vatavusv@artcollege.ru is26dzyrava@artcollege.ru>
2024-02-08 11:50:20 +0300

    Добавьте файлы проекта.

---
+++ GitLabAppMarkov/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabAppMarkov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение параметра X: ");
+            string s = Console.ReadLine();
+            double x = double.Parse(s);
+            double F = 2 * Math.Cos(x);
+            Console.WriteLine($"Значение F = {F}.");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
